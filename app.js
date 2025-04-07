c81a284b2b30713489e8b1a87d8f2860e252cf43
Ritesh Narola <riteshnarola810@gmail.com>
2025-04-07 20:00:37 +0530

    upadted new feature

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // this crated for new features
+fetch('/api/hello', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: 'John Doe' }),
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
+  
